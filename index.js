fb6f9744d458480aa142907bc75ac7887f0cefab
Casey <caseyderiso@gmail.com>
2020-11-20 14:48:31 -0600

    created test markdown file with input

--- index.js
+++ index.js
@@ -1,18 +1,54 @@
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // array of questions for user
-const questions = [
-
-];
+const questions = [];
 
 // function to write README file
 function writeToFile(fileName, data) {
+  fs.writeFile(`./${fileName}.md`, data, (err) => {
+    if (err) throw err;
+    console.log("Done!");
+  });
 }
 
 // function to initialize program
 function init() {
-    console.log(inquirer);
-}
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title of your project?",
+      validate: (projectTitle) => {
+        if (projectTitle) {
+          return true;
+        } else {
+          console.log("Please enter a title for your project!");
+          return flase;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Please provide a description of the project",
+      validate: (projectDesc) => {
+        if (projectDesc) {
+          return true;
+        } else {
+          console.log("Please enter a project description!");
+          return false;
+        }
+      },
+    },
+  ])
+
+};
 
 // function call to initialize program
-init();
+init()
+.then(readmeData => {
+    const content = readmeData.description;
+    const title = readmeData.title;
+    writeToFile(title, content)
+});
---
+++ snake-bite.md
@@ -0,0 +1,1 @@
+don't step on snaek
